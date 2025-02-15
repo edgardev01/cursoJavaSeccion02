a8915311163eeee423d32c156cfaf3fc3e04186d
EOOSORIO <edgar.osorio@proveedoresgnp.mx>
2025-02-15 16:43:34 -0600

    modelo

--- src/main/java/com/andres/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/andres/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.andres.curso.springboot.webapp.springboot_web.models.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details")
     public String details( Model model ) {
 
+        User user = new User( "Andres", "Guzman");
+
         model.addAttribute("title", "Hola Mundo Spring Boot");
-        model.addAttribute("name", "Edgar");
-        model.addAttribute("lastname", "Sanchez");
+        model.addAttribute("user", user);
+
 
         return "details";
     }
--- src/main/java/com/andres/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/andres/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,5 +1,33 @@
 package com.andres.curso.springboot.webapp.springboot_web.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andres.curso.springboot.webapp.springboot_web.models.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
 public class UserRestController {
+
+    @GetMapping(path = "/details")
+    public Map<String , Object> details() {
+
+        User user = new User( "Andres", "Guzman");
+
+        Map<String , Object> body = new HashMap<>();
+
+        body.put("title", "Hola Mundo Spring Boot");
+        body.put("user", user);
+
+
+        return body;
+    }
+    
+
 
 }
